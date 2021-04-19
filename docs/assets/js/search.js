688a38546e5e4a8bdb9ca0dda6b802eb72475891
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2021-04-19 23:05:09 +0100

    Añadadida dependecia de Typedoc

---
+++ docs/assets/js/search.js
@@ -0,0 +1,1 @@
+window.searchData = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"return_result","url":"modules.html#return_result","classes":"tsd-kind-function"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,2.877]],["parent/0",[]]],"invertedIndex":[["return_result",{"_index":0,"name":{"0":{}},"parent":{}}]],"pipeline":[]}}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+/**
+ * Mensaje en pantalla
+ */
 console.log('Hola');
+
+/**
+ * Devuelve un resultado
+ * @param parametro parametro
+ * @returns {Number}
+ */
+function return_result(parametro: number) {
+  return parametro;
+}
